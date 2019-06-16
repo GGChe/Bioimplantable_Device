bbf7dfde7fb9be880ace807d87d758e2b899e072
GGaleote <gabrielgaleote@gmail.com>
2019-06-16 18:38:58 +0100

    Circuits Updated

---
+++ Designs/Electronic_Design/Final_NCP161_Package2_antenna_8MHz/gerber/Final_v2.sol
@@ -0,0 +1,16 @@
+G04 EAGLE Gerber X2 export*
+%TF.Part,Single*%
+%TF.FileFunction,Other,Solder side*%
+%TF.FilePolarity,Positive*%
+%TF.GenerationSoftware,Autodesk,EAGLE,8.6.3*%
+%TF.CreationDate,2019-06-16T17:30:36Z*%
+G75*
+%MOMM*%
+%FSLAX34Y34*%
+%LPD*%
+%AMOC8*
+5,1,8,0,0,1.08239X$1,22.5*%
+G01*
+
+
+M02*
